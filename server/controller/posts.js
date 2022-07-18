179d2342893b3865db3978be66e0fdb3bb54c973
Timmy1011 <timmycdc@gmail.com>
2022-07-18 21:28:23 +0800

    more basic files and folders

--- server/controller/posts.js
+++ server/controller/posts.js
@@ -1,3 +1,23 @@
-export const getPosts = (req, res) => {
-    res.send("This Works");
+import PostMessage from "../models/postMessage.js";
+
+export const getPosts = async (req, res) => {
+    try {
+        const postMessage = await PostMessage.find();
+
+        res.status(200).json(postMessage);
+    } catch(err){
+        res.status(404).json({ message: err.message});
+    }
 }
+
+export const createPost = async (req, res) => {
+    const post = req.body;
+    const newPost = new PostMessage.findById(post);
+
+    try {
+        await newPost.save();
+
+        res.status(201).json(newPost);
+    } catch(err){
+        res.status(409).json({ message: err.message});
+    }}
